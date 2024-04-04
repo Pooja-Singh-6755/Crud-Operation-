4c4c1351927926ca5e606638fc1fcc7e02ea2408
Pooja-Singh-6755 <163822849+Pooja-Singh-6755@users.noreply.github.com>
2024-04-04 19:09:04 +0530

    Add files via upload

---
+++ poojadatabase/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace poojadatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button1;
+            System.Windows.Forms.Button button3;
+            System.Windows.Forms.Button button4;
+            System.Windows.Forms.Button button5;
+            System.Windows.Forms.Button button6;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SecondName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edit = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.SystemColors.GrayText;
+            button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            button1.Location = new System.Drawing.Point(12, 276);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(140, 56);
+            button1.TabIndex = 4;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            button3.BackColor = System.Drawing.SystemColors.GrayText;
+            button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            button3.Location = new System.Drawing.Point(158, 276);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(140, 56);
+            button3.TabIndex = 9;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            button4.BackColor = System.Drawing.SystemColors.GrayText;
+            button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            button4.Location = new System.Drawing.Point(314, 128);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(95, 30);
+            button4.TabIndex = 10;
+            button4.Text = "fetch";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            button5.BackColor = System.Drawing.SystemColors.GrayText;
+            button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            button5.Location = new System.Drawing.Point(66, 338);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(140, 56);
+            button5.TabIndex = 11;
+            button5.Text = "Delete";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            button6.BackColor = System.Drawing.SystemColors.GrayText;
+            button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            button6.Location = new System.Drawing.Point(1047, 49);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(140, 39);
+            button6.TabIndex = 12;
+            button6.Text = "Search";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.WhiteSpace;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(26, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(12, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 227);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(12, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(383, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AccessibleRole = System.Windows.Forms.AccessibleRole.WhiteSpace;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(45, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ID";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(175, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.FirstName,
+            this.SecondName,
+            this.Edit,
+            this.delete});
+            this.dataGridView1.Location = new System.Drawing.Point(432, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(788, 336);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.Width = 120;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.MinimumWidth = 6;
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 125;
+            // 
+            // SecondName
+            // 
+            this.SecondName.HeaderText = "SecondName";
+            this.SecondName.MinimumWidth = 6;
+            this.SecondName.Name = "SecondName";
+            this.SecondName.Width = 125;
+            // 
+            // Edit
+            // 
+            this.Edit.HeaderText = "Edit";
+            this.Edit.MinimumWidth = 6;
+            this.Edit.Name = "Edit";
+            this.Edit.Width = 125;
+            // 
+            // delete
+            // 
+            this.delete.HeaderText = "delete";
+            this.delete.MinimumWidth = 6;
+            this.delete.Name = "delete";
+            this.delete.Width = 125;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(655, 60);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(386, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AccessibleRole = System.Windows.Forms.AccessibleRole.WhiteSpace;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(655, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 26);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "seach";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "ALL",
+            "ID",
+            "FirstName",
+            "SecondName"});
+            this.comboBox3.Location = new System.Drawing.Point(456, 58);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AccessibleRole = System.Windows.Forms.AccessibleRole.WhiteSpace;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label5.Location = new System.Drawing.Point(456, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 26);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Column ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1232, 450);
+            this.Controls.Add(button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(button6);
+            this.Controls.Add(button5);
+            this.Controls.Add(button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SecondName;
+        private System.Windows.Forms.DataGridViewButtonColumn Edit;
+        private System.Windows.Forms.DataGridViewButtonColumn delete;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ poojadatabase/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace poojadatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1.address of SQL Server and Database
+            string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+            // 2. establish Connection
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            // 3.open Connection
+            con.Open();
+
+            // 4.prepare query 
+            string FirstName= textBox1.Text;
+            string LastName= textBox2.Text;
+         string Query =  "INSERT INTO names(FirstName, SecondName) values('"+FirstName+"', '"+LastName+"')";
+            SqlCommand cmd = new SqlCommand(Query, con);
+
+
+            // 5. execute query
+            cmd.ExecuteNonQuery();
+
+
+
+            // 6. close connection
+            con.Close();
+
+            MessageBox.Show("DATA HAS BEEN SAVED !!");
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //address of sql server and database(connecting string)
+            string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+
+            //establish connection  (c# sqlconnection class)
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            //open connection 
+            con.Open();
+
+            // prepare query 
+            string query = "Select * from Names";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            //Execute query 
+             var reader = cmd.ExecuteReader();
+
+            dataGridView1.Rows.Clear();     //reduce duplicate data show
+
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add(reader["ID"], reader["FirstName"].ToString().ToUpper() , reader["SecondName"].ToString().ToUpper() , "Edit");
+            }
+            
+            
+            // method 1 ( its is not alter data)
+          //  DataTable table = new DataTable();
+            //table.Load(reader);
+
+         // dataGridView1.DataSource = table;
+
+
+            //close connection 
+            con.Close();        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // adress the sql server and database
+            string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+
+            // established connection 
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            //open connection
+            con.Open();
+
+            //prepare query 
+            string nameID = textBox3.Text;
+            string FirstName = textBox1.Text;
+            string LastName = textBox2.Text;
+            string Query = "update names set FirstName='"+FirstName+ "' , SecondName='"+LastName+"' where ID = '"+nameID+"'";
+            SqlCommand cmd = new SqlCommand(Query, con);
+
+
+            //execute query
+            cmd.ExecuteNonQuery();
+
+            //close connection 
+            con.Close();
+
+            MessageBox.Show("DATA UPDATE");
+            textBox3.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            // adress the sql server and database
+            string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+
+            // established connection 
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            //open connection
+            con.Open();
+
+            //prepare query 
+            string nameID = textBox3.Text;
+           
+            string Query = "select * from names where ID='"+nameID+"'";
+            SqlCommand cmd = new SqlCommand(Query, con);
+
+
+            //execute query
+            var reader = cmd.ExecuteReader();
+
+
+            if (reader.Read())
+            {
+                textBox1.Text = reader["FirstName"].ToString();
+                textBox2.Text = reader["SecondName"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("not found");
+            }
+
+            //close connection 
+            con.Close();
+
+           
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if ( e.ColumnIndex == 3 && e.RowIndex > -1)
+            {
+                textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            }
+            else if (e.ColumnIndex == 4 && e.RowIndex > -1)
+            {
+                string nameID = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+              if(  MessageBox.Show(" Do you deleted your data ?" , "confirm deleted " , MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    // adress the sql server and database
+                    string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+
+                    // established connection 
+                    SqlConnection con = new SqlConnection(ConnectionString);
+
+                    //open connection
+                    con.Open();
+
+                    //prepare query 
+                  
+                    string Query = "delete from names where ID = '" + nameID + "'";
+                    SqlCommand cmd = new SqlCommand(Query, con);
+
+
+                    //execute query
+                    cmd.ExecuteNonQuery();
+
+                    //close connection 
+                    con.Close();
+
+                    MessageBox.Show("DATA DELETED");
+                    textBox3.Text = "";
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+
+
+                    button2_Click(this, e);   // update table without any refresh
+
+                }
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // adress the sql server and database
+            string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+
+            // established connection 
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            //open connection
+            con.Open();
+
+            //prepare query 
+            string nameID = textBox3.Text;
+         
+            string Query = "delete from names where ID = '"+nameID+"'";
+            SqlCommand cmd = new SqlCommand(Query, con);
+
+
+            //execute query
+            cmd.ExecuteNonQuery();
+
+            //close connection 
+            con.Close();
+
+            MessageBox.Show("DATA DELETED");
+            textBox3.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+
+            button2_Click(this, e);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //address of sql server and database(connecting string)
+            string ConnectionString = "Data Source=LAPTOP-8HFB67PS\\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;Encrypt=False";
+
+
+            //establish connection  (c# sqlconnection class)
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            //open connection 
+            con.Open();
+
+            // prepare query 
+            string search = textBox4.Text;
+
+            string query = "select * from names where firstName like '%"+search+"%' or SecondName like '%"+search+"%';";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            //Execute query 
+            var reader = cmd.ExecuteReader();
+
+            dataGridView1.Rows.Clear();     //reduce duplicate data show
+
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add(reader["ID"], reader["FirstName"].ToString().ToUpper(), reader["SecondName"].ToString().ToUpper(), "Edit");
+            }
+
+            con.Close();
+        }
+    }
+}
